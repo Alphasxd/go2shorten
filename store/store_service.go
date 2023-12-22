35838b0f79bc274795a819ea5c87ff871f0fd3a2
Sheldon <alphasxd@outlook.com>
2023-12-22 12:16:10 +0800

    store_service

---
+++ store/store_service.go
@@ -0,0 +1,57 @@
+package store
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+)
+
+// StorageService 定义一个结构体，用来存储redis的客户端
+type StorageService struct {
+	redisClient *redis.Client
+}
+
+// 定义一个全局变量，用来存储redis的客户端和上下文
+var (
+	storeService = &StorageService{}
+	ctx          = context.Background()
+)
+
+// CacheDuration 定义缓存期限，实际场景中不需要定义，应该使用 LRU 算法
+const CacheDuration = 6 * time.Hour
+
+// InitializeStore 初始化 StoreService
+func InitializeStore() *StorageService {
+	redisClient := redis.NewClient(&redis.Options{
+		Addr:     "redis-11708.c292.ap-southeast-1-1.ec2.cloud.redislabs.com:11708",
+		Password: "G6Njk0oCWMqiyQ3FHGUtuTiNpnvmk9j2",
+		DB:       0,
+	})
+
+	pong, err := redisClient.Ping(ctx).Result()
+	if err != nil {
+		panic(fmt.Sprintf("Failed to connect to Redis: %v", err))
+	}
+	fmt.Printf("Connected to Redis: %v\n", pong)
+	storeService.redisClient = redisClient
+	return storeService
+}
+
+// SaveUrlMapping 存储原始URL和短链接的映射
+func SaveUrlMapping(shortUrl string, originalUrl string, userId string) {
+	err := storeService.redisClient.Set(ctx, shortUrl, originalUrl, CacheDuration).Err()
+	if err != nil {
+		panic(fmt.Sprintf("Failed saving key url | Error: %v - shortUrl: %s - originalUrl: %s\n", err, shortUrl, originalUrl))
+	}
+}
+
+// RetrieveInitialUrl 根据短链接获取原始URL
+func RetrieveInitialUrl(shortUrl string) string {
+	result, err := storeService.redisClient.Get(ctx, shortUrl).Result()
+	if err != nil {
+		panic(fmt.Sprintf("Failed RetrieveInitialUrl url | Error: %v - shortUrl: %s\n", err, shortUrl))
+	}
+	return result
+}
